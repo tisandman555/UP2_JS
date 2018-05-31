428e4e68c8d4be219ea76d58bba00915909d5d48
decemberpei <decemberpei@gmail.com>
2018-05-31 14:25:15 +0800

    update aws mqtts lab code

--- aws-mqtts-lab/read_th_sensor.js
+++ aws-mqtts-lab/read_th_sensor.js
@@ -8,12 +8,14 @@ mraa.addSubplatform(mraa.GROVEPI,"0");
 var sensor1 = require('jsupm_th02');
 var th02 = new sensor1.TH02();
 
-function read_sensor()
-{
-	//get sensor data
-	temp = th02.getTemperature();
-	humi = th02.getHumidity();
-	return {'temperature':temp,'humidity':humi};
+function read_sensor(){
+   //get sensor data
+   temp = th02.getTemperature();
+   humi = th02.getHumidity();
+   var timestamp = Math.floor(new Date()/1000);
+   var result = {'temperature':temp,'humidity':humi, 'timestamp':timestamp};
+   console.log(result)
+   return result;
 }
 
 // export the function for other module use
