a90d99df720d87a0e678143a96896a52e740f172
Pei <fanjiang.pei@intel.com>
2018-06-21 08:44:52 +0800

    update aws mqtts lab code and enable the subscribe mqtts mesg feature

--- aws-mqtts-lab/aws_mqtts_lab.js
+++ aws-mqtts-lab/aws_mqtts_lab.js
@@ -11,22 +11,28 @@ const read_sensor = require('./read_th_sensor');
 // node device-example.js --host-name=a1lbizsabp5xcz.iot.us-west-2.amazonaws.com --private-key=UP2-Fanjiang-P_Plus.private.key --client-certificate=UP2-Fanjiang-P_Plus.cert.pem --ca-certificate=root-CA.crt
 
 //begin module
-const my_topic = 'com/intel/fpei/up2';
+const my_topic = 'com/intel/fpei/up02';
 const interval = 5000; // 5000ms
 
 const device = deviceModule({
-   keyPath: 'UP2-Fanjiang-P_Plus.private.key',
-   certPath: 'UP2-Fanjiang-P_Plus.cert.pem',
+   keyPath: 'test1.private.key',
+   certPath: 'test1.cert.pem',
    caPath: 'root-CA.crt',
-   clientId: 'fpei',
-   region: 'us-west-2',
+   clientId: 'test1',
+   region: 'cn-north-1',
    baseReconnectTimeMs: 4000,
    keepalive: 0,
    protocol: 'mqtts',
    port: 8883,
-   host: 'a1lbizsabp5xcz.iot.us-west-2.amazonaws.com',
+   host: 'a3a396o25ywixx.iot.cn-north-1.amazonaws.com.cn',
    debug: true
 });
+
+// uncomment following code block to subscribe to the MQTTs topic $my_topic, and print the mesgs on arrive. 
+//device.subscribe(my_topic);
+//device.on('message', function(topic, payload) {
+//      console.log('message', topic, payload.toString());
+//  });
 
 // publish sensor data every $interval.
 var timeout = setInterval(function() {
@@ -48,7 +54,4 @@ device.on('offline', function() {
    });
 device.on('error', function(error) {
       console.log('error', error);
-   });
-device.on('message', function(topic, payload) {
-      console.log('message', topic, payload.toString());
    });
